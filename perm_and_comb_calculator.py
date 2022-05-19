aff3f621e2c714aa3358d59fda6900937f57f9bd
agear123 <103547186+agear123@users.noreply.github.com>
2022-05-19 15:48:29 -0500

    Add files via upload

---
+++ perm_and_comb_calculator.py
@@ -0,0 +1,58 @@
+'''
+Number of possible permutations and combinations
+from a number of n choosings from a set of size k.  
+'''
+import math
+
+def permutations_calc():
+    '''
+    Return number of possible permutations.
+
+    Parameters
+    ----------
+    n : int
+        Size of subset chosen from superset.
+    k : int
+        Superset size from which subset of size n is chosen from.
+
+    Returns
+    -------
+    permutations : int 
+                   Number of possible permutations.
+
+    '''
+    
+    n = int(input('What is the value of n? '))
+    
+    if n < 0:
+        raise ValueError('n must be an integer greater than zero.')
+    
+    k = int(input('What is the value of k? '))
+    
+    if k > n:
+        raise ValueError('k must be a positive integer less than or equal to n.')
+    
+    permutations = math.factorial(n) / math.factorial(n - k) 
+    permutations = int(permutations)
+    
+    '''Note permutations and combinations formulas have common inputs.'''
+    
+    combinations = math.factorial(n) / math.factorial(k) * math.factorial(n - k) 
+    combinations = int(combinations)
+    print()
+    print('{} possible permutations'.format(permutations))
+    print('{} possible combinations'.format(combinations))
+
+    return permutations
+
+permutations_calc()
+
+
+
+
+
+
+
+
+
+
